474817c5caf8f54cd5732ad22ee593094fe957d9
HadiKhai <35467011+HadiKhai@users.noreply.github.com>
2021-07-21 17:31:16 +0200

    feat: check copy tasks for recyclebin

---
+++ src/graasp-recycle-bin-errors.ts
@@ -0,0 +1,37 @@
+import { GraaspErrorDetails, GraaspError } from 'graasp';
+
+export class GraaspItemTagsError implements GraaspError {
+  name: string;
+  code: string
+  message: string;
+  statusCode?: number;
+  data?: unknown;
+  origin: 'core' | 'plugin';
+
+  constructor({ code, statusCode, message }: GraaspErrorDetails, data?: unknown) {
+    this.name = code;
+    this.code = code;
+    this.message = message;
+    this.statusCode = statusCode;
+    this.data = data;
+    this.origin = 'plugin';
+  }
+}
+
+export class CopyToRecycleBin extends GraaspItemTagsError {
+  constructor(data?: unknown) {
+    super({ code: 'GRBERR001', statusCode: 404, message: 'Cannot Copy Item to Recycle Bin' }, data);
+  }
+}
+
+export class CopyRecycleBin extends GraaspItemTagsError {
+  constructor() {
+    super({ code: 'GRBERR002', statusCode: 404, message: 'Cannot Copy Recycle Bin' });
+  }
+}
+
+export class MoveRecycleBin extends GraaspItemTagsError {
+  constructor() {
+    super({ code: 'GRBERR003', statusCode: 404, message: 'Cannot Move Recycle Bin' });
+  }
+}
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,8 @@
 import {FastifyPluginAsync} from "fastify";
 import {BaseItem} from "./base-item";
-import {IdParam, PreHookHandlerType} from "graasp";
+import {IdParam, Item, ItemCopyHookHandlerExtraData, Member, PreHookHandlerType} from "graasp";
+import {CopyRecycleBin, CopyToRecycleBin} from "./graasp-recycle-bin-errors";
+import {RECYCLE_BIN_TYPE} from "./constants";
 
 const plugin: FastifyPluginAsync = async (fastify) => {
 
@@ -10,12 +12,48 @@ const plugin: FastifyPluginAsync = async (fastify) => {
     itemMemberships: { taskManager: itemMembershipTaskManager },
     taskRunner: runner } = fastify;
 
-  fastify.patch<{ Params: IdParam}> (
+
+  /**
+   * Check if an item is being copied to the recycle bin
+   * @param copy Item copy (before being saved)
+   * @param member Member triggering the copy task - used to get recycleBin
+   */
+  const preventCopyToRecycleBin: PreHookHandlerType<Item> =
+    async (copy: Item, member: Member ) => {
+      const { path: copyPath } = copy;
+
+      const { extra } = member;
+      const recycleBin = extra['recycleBin']
+
+      if(!recycleBin) return
+
+      const recycleBinId = recycleBin['id'];
+      const getRecycleBinTask = itemTaskManager.createGetTask(member,recycleBinId);
+      const recycleBinItem = await runner.runSingle(getRecycleBinTask);
+      const { path: recycleBinPath } = recycleBinItem;
+      if(copyPath.includes(recycleBinPath)) throw new CopyToRecycleBin(copy.id)
+
+    };
+  runner.setTaskPreHookHandler(itemTaskManager.getCopyTaskName(), preventCopyToRecycleBin);
+
+  /**
+   * Check if the recycle bin is being copied
+   * @param copy Item copy (before being saved)
+   */
+  const preventCopyOfRecycleBin: PreHookHandlerType<Item> =
+    async (copy: Item) => {
+      const { type } = copy;
+
+      if(type=== RECYCLE_BIN_TYPE) throw new CopyRecycleBin()
+    };
+  runner.setTaskPreHookHandler(itemTaskManager.getCopyTaskName(), preventCopyOfRecycleBin);
+
+  fastify.post<{ Params: IdParam}> (
     '/:id/recycle',
     async ({member,params: {id: itemId},log})=> {
-      const item = itemTaskManager.createGetTask(member,itemId);
 
-      if(!item) throw new Error("Item Not Found");
+      const itemTask = itemTaskManager.createGetTask(member,itemId);
+      const item = await runner.runSingle(itemTask,log);
 
       const {extra, name,id: memberId} = member;
       let recycleBin;
@@ -28,7 +66,7 @@ const plugin: FastifyPluginAsync = async (fastify) => {
         const recycleBinItem = new BaseItem(
           `${name}'s Recycle Bin`,
           "Recycle Bin",
-          "recycleBin",
+          RECYCLE_BIN_TYPE,
           {},
           memberId);
         const createRecycleBinTask = itemTaskManager.createCreateTask(member,recycleBinItem);
