e21ff20c60c64d02fe09ca24682d71adfea40348
pyphilia <pyphilia@gmail.com>
2021-09-07 15:51:09 +0200

    refactor: add more details in schema

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,8 +1,34 @@
+export default {
+  $id: 'http://graasp.org/recycle-bin/',
+  definitions: {
+    // item properties to be returned to the client
+    item: {
+      type: 'object',
+      properties: {
+        id: { type: 'string' },
+        name: { type: 'string' },
+        description: { type: ['string', 'null'] },
+        type: { type: 'string' },
+        path: { type: 'string' },
+        extra: {
+          type: 'object',
+          additionalProperties: true
+        },
+        creator: { type: 'string' },
+        createdAt: {},
+        updatedAt: {}
+      },
+      additionalProperties: false
+    }
+  }
+};
+
 // schema for getting recycled items
 const getRecycledItems = {
   response: {
     200: {
       type: 'array',
+      items: { $ref: 'http://graasp.org/recycle-bin/#/definitions/item' }
       // todo: use global schema to force response with item schema
     },
   }
