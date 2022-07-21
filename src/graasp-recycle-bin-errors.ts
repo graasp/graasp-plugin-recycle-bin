1de0d21fd4f89bd552a6e451035d48cf24b1212d
pyphilia <pyphilia@gmail.com>
2022-07-21 10:19:41 +0200

    refactor: update errors

--- src/graasp-recycle-bin-errors.ts
+++ src/graasp-recycle-bin-errors.ts
@@ -1,37 +1,34 @@
-import { BaseGraaspError } from '@graasp/sdk';
+import { ErrorFactory } from '@graasp/sdk';
 
 import { PLUGIN_NAME } from './constants';
 
-export class CannotCopyRecycledItem extends BaseGraaspError {
-  origin = PLUGIN_NAME;
+const GraaspError = ErrorFactory(PLUGIN_NAME);
+
+export class CannotCopyRecycledItem extends GraaspError {
   constructor(data?: unknown) {
     super({ code: 'GRBERR002', statusCode: 400, message: 'Cannot copy recycled item' }, data);
   }
 }
 
-export class CannotMoveRecycledItem extends BaseGraaspError {
-  origin = PLUGIN_NAME;
+export class CannotMoveRecycledItem extends GraaspError {
   constructor(data?: unknown) {
     super({ code: 'GRBERR003', statusCode: 400, message: 'Cannot move recycled item' }, data);
   }
 }
 
-export class CannotDeleteRecycleItem extends BaseGraaspError {
-  origin = PLUGIN_NAME;
+export class CannotDeleteRecycleItem extends GraaspError {
   constructor(data?: unknown) {
     super({ code: 'GRBERR004', statusCode: 400, message: 'Cannot delete recycled item' }, data);
   }
 }
 
-export class CannotGetRecycledItem extends BaseGraaspError {
-  origin = PLUGIN_NAME;
+export class CannotGetRecycledItem extends GraaspError {
   constructor(data?: unknown) {
     super({ code: 'GRBERR007', statusCode: 400, message: 'Cannot get recycled item' }, data);
   }
 }
 
-export class InvalidItemStatus extends BaseGraaspError {
-  origin = PLUGIN_NAME;
+export class InvalidItemStatus extends GraaspError {
   constructor(data?: unknown) {
     super({ code: 'GRBERR008', statusCode: 400, message: 'Invalid item status' }, data);
   }
