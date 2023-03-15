12457e84c8735dfcbdb29b07a70027ceb3484ecf
Kim Lan Phan Hoang <pyphilia@gmail.com>
2023-03-15 14:56:17 +0100

    fix: update db schema update constraint
    allows rows to update if item moves

--- db-schema.sql
+++ db-schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "recycled_item" (
   "id" uuid PRIMARY KEY UNIQUE NOT NULL DEFAULT uuid_generate_v4(), -- generated programatically and passed on insertion
-  "item_id" uuid UNIQUE NOT NULL REFERENCES "item" ("id") ON DELETE CASCADE,
+  "item_id" uuid UNIQUE NOT NULL REFERENCES "item" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
   "item_path" ltree UNIQUE NOT NULL,
   "creator" uuid REFERENCES "member" ("id") ON DELETE SET NULL, -- don't remove item - set creator to NULL
   "created_at" timestamp NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc')
